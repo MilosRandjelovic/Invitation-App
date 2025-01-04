2250d0addb1a25fa365460d8227f8012f068cb2d
MilosRandjelovic <mrandjelovic1979@gmail.com>
2025-01-04 16:52:20 +0100

    Setup, install all packages, components, actions, pages, authentication... welcome!

---
+++ app/utils/strings.ts
@@ -0,0 +1,20 @@
+export const strings = {
+    title: "Birthday RSVP",
+    description:
+      "I am excited to celebrate my special day with you! Please let me know if you can make it.",
+    eventDate: "2025-08-23", // YYYY-MM-DD format
+    eventDateLabel: "Event Date",
+    eventLocation: "Pivnica Nemir, Nis, Ucitelj Tasina 17",
+    eventLocationLabel: "Event Location",
+    nameLabel: "Full Name",
+    emailLabel: "Email Address",
+    accompanyLabel: "Number of Guests",
+    rsvpLabel: "Will you attend?",
+    yesOption: "Yes, I'll be there!",
+    noOption: "Sorry, I can't make it",
+    submitButton: "Send RSVP",
+    thankYouMessage: "Thank you for your response!",
+    viewOnMapButton: "View Location on Google Maps",
+    sendFromEmail: "onboarding@resend.dev",
+    sendToEmail: process.env.EMAIL_TO,
+  };
